c9d2fd206aa43d15c84ec9cd9ce7da31b3e85951
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2023-11-18 20:21:31 +0100

    switch to react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main>
+      nav
+      <section>Hero</section>
+      <section>PopularProducts</section>
+      <section>SuperQuality</section>
+      <section>Services</section>
+      <section>SpecialOffers</section>
+      <section>CustomerReviews</section>
+      <section>Subscribe</section>
+      <section>Footer</section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Sections/index.ts
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffers'
+import SuperQuality from './SuperQuality'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+import CustomerReviews from './CustomerReviews'
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  Subscribe,
+  Footer,
+  CustomerReviews,
+}
